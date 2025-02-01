132eeacc74cde33c1f3b5586646632b84713c0e5
Andy <obrezkov.andy@gmail.com>
2025-02-01 13:46:53 +0300

    Add accordion

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import { burgerMenu } from "./modules/burger-menu";
+import { accordion } from "./modules/accordion";
 import { animations } from "./modules/animations";
 
 burgerMenu();
+accordion();
 animations();
